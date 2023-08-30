595613ec5733b35baacde3a5b5caf553db81e2f2
Jwhyee <kawnsdud@naver.com>
2023-08-30 23:40:46 +0900

    [BOJ][SIL1] - 유성

--- src/ka/impl/BOJ_10703.java
+++ src/ka/impl/BOJ_10703.java
@@ -28,8 +28,8 @@ X.....X
  * 링크 : https://www.acmicpc.net/problem/10703
  */
 public class BOJ_10703 {
-    static char[][] map;
-    static int R, S, xCnt;
+    static char[][] map, copy;
+    static int R, S, meteorTotalCnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -39,15 +39,17 @@ public class BOJ_10703 {
         R = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
         map = new char[R][S];
+        copy = new char[R][S];
 
         for (int i = 0; i < R; i++) {
             char[] arr = br.readLine().toCharArray();
             for (int j = 0; j < S; j++) {
                 char c = arr[j];
                 if (c == 'X') {
-                    xCnt++;
+                    meteorTotalCnt++;
                 }
                 map[i][j] = c;
+                copy[i][j] = c;
             }
         }
 
@@ -64,34 +66,48 @@ public class BOJ_10703 {
     }
 
     private static void meteor() {
-        int m = S;
-        while (m-- > 0) {
-            // 유성을 밑으로 한 칸 이동
-            int tempCnt = 0;
-            for (int i = 0; i < R; i++) {
-                for (int j = 0; j < S; j++) {
-                    char c = map[i][j];
+
+        int h = R;
+        boolean flag = false;
+        int cnt = 0;
+
+        // 유성을 밑으로 한 칸씩 이동하는 반복문
+        while (true) {
+            // 밑에서부터 유성을 한 칸씩 밑으로 옮김
+            outer : for (int i = R - 1; i >= 0; i--) {
+                for (int j = S - 1; j >= 0; j--) {
+                    char c = copy[i][j];
                     if (c == 'X') {
-                        if (map[i + 1][j] != '#') {
-                            tempCnt++;
+                        if (copy[i + 1][j] != '#') {
+                            copy[i][j] = '.';
+                            copy[i + 1][j] = c;
+                        } else {
+                            cnt--;
+                            flag = true;
+                            break outer;
                         }
                     }
+
                 }
             }
+            if (flag) {
+                for (int z = 0; z <= cnt; z++) {
+                    for (int i = R - 1; i >= 0; i--) {
+                        for (int j = S - 1; j >= 0; j--) {
+                            char c = map[i][j];
+                            if (c == 'X') {
+                                if (map[i + 1][j] != '#') {
+                                    map[i][j] = '.';
+                                    map[i + 1][j] = c;
+                                }
+                            }
 
-            if (tempCnt == xCnt) {
-                for (int i = R - 1; i >= 0; i--) {
-                    for (int j = S - 1; j >= 0; j--) {
-                        char c = map[i][j];
-                        if (c == 'X') {
-                            map[i][j] = '.';
-                            map[i + 1][j] = c;
                         }
-
                     }
                 }
-            } else {
                 break;
+            } else {
+                cnt++;
             }
         }
     }
