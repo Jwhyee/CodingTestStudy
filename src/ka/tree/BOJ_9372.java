028e93ac19cdba55ca8a48a524a42a9598402598
Jwhyee <kawnsdud@naver.com>
2023-10-13 23:24:36 +0900

    [BOJ][SIL4] - 상근이의 여행

---
+++ src/ka/tree/BOJ_9372.java
@@ -0,0 +1,32 @@
+package ka.tree;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_9372 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            for (int i = 0; i < m; i++) {
+                br.readLine();
+            }
+            bw.append((n - 1) + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
